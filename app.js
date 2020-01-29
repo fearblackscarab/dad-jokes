b1f187781379c5aef079e3d97e84fac4443771da
FearBlackScarab <fearblackscarab@gmail.com>
2020-01-28 19:09:24 -0600

    practice

---
+++ app.js
@@ -0,0 +1,28 @@
+let joke= document.getElementById('box');
+let output= document.getElementsById('output')
+let i='0';
+let t='3';
+var joke1=joke1
+var joke2=joke2
+var joke3=joke3
+var joke4=joke4
+var joke5=joke5
+var joke6=joke6
+var joke7=joke7
+var joke8=joke8
+var joke9=joke9
+var joke10
+
+grid = [joke1
+    ,joke2
+    ,joke3
+    ,joke4
+    ,joke5
+    ,joke6
+    ,joke7
+    ,joke8
+    ,joke9
+    ,joke10
+];
+
+
